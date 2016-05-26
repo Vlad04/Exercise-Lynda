51adf7dd8f1945ede3cfe17a6598f2950cc5cdcb
Vlad <Vlad04@users.noreply.github.com>
2016-05-25 18:35:22 -0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -15,40 +15,32 @@ int main()
     int initialTX; // Thor's starting X position
     int initialTY; // Thor's starting Y position
     scanf("%d%d%d%d", &lightX, &lightY, &initialTX, &initialTY);
+
     int Thor_X=initialTX;
     int Thor_Y=initialTY;
+
     // game loop
     while (1) {
         int remainingTurns; // The remaining amount of turns Thor can move. Do not remove this line.
         scanf("%d", &remainingTurns);
-        char *Pos_X="";
-        char *Pos_Y="";
-        // Write an action using printf(). DON'T FORGET THE TRAILING \n
-        // To debug: fprintf(stderr, "Debug messages...\n");
-        if(Thor_X>lightX)
-        {
-         Pos_X="W";   
-         Thor_X--;
+        char *Pos_X = "";
+        char *Pos_Y = "";
+        
+        if (Thor_X > lightX) {
+            Pos_X = "W";
+            Thor_X--;
+        } else if (Thor_X < lightX) {
+            Pos_X = "E";
+            Thor_X++;
         }
-        else if(Thor_X<lightX)
-        {
-         Pos_X="E";
-         Thor_X++;
+        if (Thor_Y > lightY) {
+            Pos_Y = "N";
+            Thor_Y--;
+        } else if (Thor_Y < lightY) {
+            Pos_Y = "S";
+            Thor_Y++;
         }
-        else if(Thor_Y>lightY)
-        {
-         Pos_Y="N";
-         Thor_Y--;
-        }
-        else if(Thor_Y<lightY)
-        {
-         Pos_Y="S";
-         Thor_Y++;
-        }
-    
-
-        // A single line providing the move to be made: N NE E SE S SW W or NW
-        printf("%s%s\n",Pos_X,Pos_Y);
+        printf("%s%s\n", Pos_Y, Pos_X);
     }
 
     return 0;
